9a0bf4ab94237437e8199e01362398c36771f898
seervivarsha24 <seervivarsha24@gmail.com>
2023-03-25 20:08:56 +0530

    Successfully done

--- src/main/java/com/driver/services/impl/PaymentServiceImpl.java
+++ src/main/java/com/driver/services/impl/PaymentServiceImpl.java
@@ -16,6 +16,39 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public Payment pay(Integer reservationId, int amountSent, String mode) throws Exception {
+        Reservation reservation=reservationRepository2.findById(reservationId).get();
+        Spot spot=reservation.getSpot();
 
+
+        int bill=reservation.getNumberOfHours()*spot.getPricePerHour();
+        if(amountSent<bill){
+            throw new Exception("Insufficient Amount");
+        }
+
+        if(mode.equalsIgnoreCase("cash") || mode.equalsIgnoreCase("card") || mode.equalsIgnoreCase("upi") ){
+            Payment payment=new Payment();
+
+            if(mode.equalsIgnoreCase("cash")){
+                payment.setPaymentMode(PaymentMode.CASH);
+            }
+            else if(mode.equalsIgnoreCase("card")){
+                payment.setPaymentMode(PaymentMode.CARD);
+            }
+            else payment.setPaymentMode(PaymentMode.UPI);
+
+            payment.setPaymentCompleted(true);
+            payment.setReservation(reservation);
+
+            reservation.setPayment(payment);
+
+
+
+            reservationRepository2.save(reservation);
+
+            return payment;
+
+
+        }
+        else throw new Exception("Payment mode not detected");
     }
 }
